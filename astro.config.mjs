81fc99ea4d541d17627e60e0395580fbb94f9300
minhquanlk <minhquanlk20022@gmail.com>
2024-03-04 22:01:17 +0700

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
-  site: `http://localhost:${process.env.PORT}`,
-  base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
-  output: "server",
-  adapter: vercel(),
-  integrations: [sitemap(), tailwind(), react()]
+    site: `http://localhost:${process.env.PORT}`,
+
+    base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
+    output: "server",
+    adapter: node({
+        mode: "middleware"
+    }),
+    integrations: [
+        sitemap(),
+        tailwind(),
+        react()
+    ],
 });
